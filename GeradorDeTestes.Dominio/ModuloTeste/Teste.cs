b4735009072effff991940266c623c325786ddb1
bernardoalvesmelo <bernardoalvesdemelo123@gmail.com>
2023-07-05 16:04:44 -0300

    correção de nome de interface de teste

--- GeradorDeTestes.Dominio/ModuloTeste/Teste.cs
+++ GeradorDeTestes.Dominio/ModuloTeste/Teste.cs
@@ -1,63 +1,80 @@
-﻿namespace GeradorDeTestes.Dominio.ModuloItem
+﻿using GeradorDeTestes.Dominio.ModuloDisciplina;
+using GeradorDeTestes.Dominio.ModuloMateria;
+using GeradorDeTestes.Dominio.ModuloQuestao;
+
+namespace GeradorDeTestes.Dominio.ModuloTeste
 {
     [Serializable]
-    public class Item : EntidadeBase<Item>
+    public class Teste : EntidadeBase<Teste>
     {
-        public string descricao;
-        public decimal valor;
+        public string titulo;
+        public bool ehRecuperacao;
+        public int quantidadeQuestoes;
+        public Disciplina disciplina;
+        public Materia materia;
+        public List<Questao> questoes;
 
-        public Item()
+        public Teste()
         {
             
         }
 
-        public Item(string descricao, decimal valor)
+        public Teste(string titulo, bool ehRecuperacao, Disciplina disciplina, Materia materia, List<Questao> questoes)
         {
-            this.descricao = descricao;
-            this.valor = valor;
+            this.titulo = titulo;
+            this.ehRecuperacao = ehRecuperacao;
+            this.disciplina = disciplina;
+            this.materia = materia;
+            this.questoes = questoes;
         }
 
-        public Item(int id, string descricao, decimal valor)
+        public Teste(int id, string titulo, bool ehRecuperacao, Disciplina disciplina, Materia materia, List<Questao> questoes)
         {
-            this.id = id;
-            this.descricao = descricao;
-            this.valor = valor;
+            this.titulo = titulo;
+            this.ehRecuperacao = ehRecuperacao;
+            this.disciplina = disciplina;
+            this.materia = materia;
+            this.questoes = questoes;
         }
 
-        public override void AtualizarInformacoes(Item registroAtualizado)
+        public override void AtualizarInformacoes(Teste registroAtualizado)
         {
             this.id = registroAtualizado.id;
-            this.descricao = registroAtualizado.descricao;
-            this.valor = registroAtualizado.valor;
+            this.titulo = registroAtualizado.titulo;
+            this.ehRecuperacao = registroAtualizado.ehRecuperacao;
+            this.disciplina = registroAtualizado.disciplina;
+            this.materia = registroAtualizado.materia;
+            this.questoes = registroAtualizado.questoes;
         }
 
         public override string[] Validar()
         {
             List<string> erros = new List<string>();
 
-            if (string.IsNullOrEmpty(descricao))
-                erros.Add("O campo 'Descrição' é obrigatório");
+            if (string.IsNullOrEmpty(titulo))
+                erros.Add("O campo 'Título' é obrigatório");
 
-            if (descricao.Length < 3 )
-                erros.Add("O campo 'Descrição' deve conter no mínimo 3 caracteres");
-
-            if (valor < 1)
-                erros.Add("O campo 'Valor' não pode receber o valor 0");
+            if (titulo.Length < 5 )
+                erros.Add("O campo 'Título' deve conter no mínimo 5 caracteres");
 
             return erros.ToArray();
         }
 
         public override string ToString()
         {
-            return $"Id: {id} Descrição: {descricao} Valor: {valor}";
+            return $"Id: {id} Título: {titulo}";
         }
 
         public override bool Equals(object? obj)
         {
-            return obj is Item item &&
-                   id == item.id &&
-                   descricao == item.descricao &&
-                   valor == item.valor;
+            return obj is Teste teste &&
+                   id == teste.id &&
+                   titulo == teste.titulo &&
+                   ehRecuperacao == teste.ehRecuperacao &&
+                   quantidadeQuestoes == teste.quantidadeQuestoes &&
+                   EqualityComparer<Disciplina>.Default.Equals(disciplina, teste.disciplina) &&
+                   EqualityComparer<Materia>.Default.Equals(materia, teste.materia) &&
+                   EqualityComparer<List<Questao>>.Default.Equals(questoes, teste.questoes);
         }
     }
 }
