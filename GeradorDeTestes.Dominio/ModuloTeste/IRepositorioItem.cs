88128e4854daf78254986e2d0afef29af9f0abf9
bernardoalvesmelo <bernardoalvesdemelo123@gmail.com>
2023-07-05 16:01:03 -0300

    implementando IRepositorioTeste

---
+++ GeradorDeTestes.Dominio/ModuloTeste/IRepositorioItem.cs
@@ -0,0 +1,11 @@
+﻿namespace GeradorDeTestes.Dominio.ModuloTeste
+{
+    public interface IRepositorioTeste
+    {
+        void Inserir(Teste novoTeste);
+        void Editar(int id, Teste teste);
+        void Excluir(Teste testeSelecionado);
+        List<Teste> SelecionarTodos();
+        Teste SelecionarPorId(int id);
+    }
+}
