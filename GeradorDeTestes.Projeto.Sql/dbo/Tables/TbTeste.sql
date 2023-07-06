f142d6afced6aff8fe958430e7dde0b1499f165e
bernardoalvesmelo <bernardoalvesdemelo123@gmail.com>
2023-07-06 12:49:11 -0300

    implementado tabelas de ModuloTeste

---
+++ GeradorDeTestes.Projeto.Sql/dbo/Tables/TbTeste.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[TbTeste]
+(
+	  [Id]            INT           IDENTITY (1, 1) NOT NULL,
+      [Titulo]        VARCHAR (200)  NOT NULL,
+      [Recuperacao]   BIT NOT NULL,
+      [Quantidade_Questoes] INT NOT NULL,
+      [Materia_Id] INT NOT NULL,
+      [Disciplina_Id] INT NOT NULL,
+      CONSTRAINT [PK_TbTeste] PRIMARY KEY CLUSTERED ([Id] ASC),
+      CONSTRAINT [FK_TbTeste_TbMateria] FOREIGN KEY ([Materia_id]) REFERENCES [dbo].[TbMateria] ([Id]),
+      CONSTRAINT [FK_TbTeste_TbDisciplina] FOREIGN KEY ([Disciplina_id]) REFERENCES [dbo].[TbDisciplina] ([Id])
+)
---
+++ GeradorDeTestes.Projeto.Sql/dbo/Tables/TbTeste_TbQuestao.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[TbTeste_TbQuestao]
+(
+	  [Teste_Id] INT NOT NULL,
+      [Questao_Id] INT NOT NULL,
+      CONSTRAINT [FK_TbTeste_TbQuestao_TbTeste] FOREIGN KEY ([Teste_id]) REFERENCES [dbo].[TbTeste] ([Id]),
+      CONSTRAINT [FK_TbTeste_TbQuestao_TbQuestao] FOREIGN KEY ([Questao_id]) REFERENCES [dbo].[TbQuestao] ([Id])
+)
