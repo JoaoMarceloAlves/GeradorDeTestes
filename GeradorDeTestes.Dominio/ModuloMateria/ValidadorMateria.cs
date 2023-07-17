00851a0893f1d5c8a1dfc051c6d3f78dfcd81c9f
bernardoalvesmelo <bernardoalvesdemelo123@gmail.com>
2023-07-17 15:40:44 -0300

    implementando aplicacao materia

---
+++ GeradorDeTestes.Dominio/ModuloMateria/ValidadorMateria.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace GeradorDeTestes.Dominio.ModuloMateria
+{
+    public class ValidadorMateria : AbstractValidator<Materia>
+    {
+        public ValidadorMateria()
+        {
+            RuleFor(x => x.Serie)
+                .NotEmpty()
+                .NotNull();
+
+            RuleFor(x => x.Nome)
+                .NotEmpty()
+                .NotNull()
+                .MinimumLength(5);
+        }
+    }
+}
